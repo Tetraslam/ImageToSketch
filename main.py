06aebc33a36d6951ea162ee2fa3f630bc41bd46e
Tetraslam <bhowmickshresht@gmail.com>
2023-01-13 09:47:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+
+# read image
+img = cv2.imread('image.jpg')
+
+# convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# apply Gaussian blur
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+# get edges
+edges = cv2.Canny(gray, 50, 150)
+
+# invert colors
+edges = cv2.bitwise_not(edges)
+
+# save sketch
+cv2.imwrite('sketch.jpg', edges)
